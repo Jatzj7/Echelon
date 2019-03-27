c9e4fc3ba66a3e2b63cc7e775887f118cd22617f
Rishit Mehta <rishitmehta1999@gmail.com>
2019-03-28 04:40:11 +0530

    Final with Download not working

--- app/src/main/java/com/example/rishitmehta/echelon/CN.java
+++ app/src/main/java/com/example/rishitmehta/echelon/CN.java
@@ -1,13 +1,77 @@
 package com.example.rishitmehta.echelon;
 
+import android.app.DownloadManager;
+import android.content.Context;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import static android.os.Environment.DIRECTORY_DOWNLOADS;
 
 public class CN extends AppCompatActivity {
+
+
+    Button down;
+    FirebaseStorage firebaseStorage;
+    StorageReference storageReference;
+
+    StorageReference ref;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cn);
+
+        down = findViewById(R.id.btDownload);
+
+        down.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                download();
+            }
+        });
+
     }
-}
+
+        public void download(){
+            storageReference= firebaseStorage.getInstance().getReference();
+            ref=storageReference.child("Unit 1 - Introduction.pdf");
+
+            ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                @Override
+                public void onSuccess(Uri uri) {
+                    String url = uri.toString();
+                    downloadfile(CN.this,"Unit 1 - Introduction",".pdf",DIRECTORY_DOWNLOADS,url);
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+
+                }
+            });
+
+        }
+
+        public void downloadfile(Context context, String fileName, String fileExtension, String destinationDirectory, String url){
+
+            DownloadManager downloadManager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+            Uri uri = Uri.parse(url);
+            DownloadManager.Request request = new DownloadManager.Request(uri);
+
+            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+            request.setDestinationInExternalFilesDir(context, destinationDirectory, fileName + fileExtension);
+            downloadManager.enqueue(request);
+        }
+    }
+
+
---
+++ app/src/main/java/com/example/rishitmehta/echelon/Dashboard.java
@@ -0,0 +1,31 @@
+package com.example.rishitmehta.echelon;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Dashboard extends Fragment {
+    public static Dashboard newInstance() {
+        Dashboard fragment = new Dashboard();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_dashboard,
+                container, false);
+
+        return rootView;
+    }
+
+
+}
